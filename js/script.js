5c1828a295876501fb1c085a96c329bb24dce77e
Rafael Alves <rafaelvitor_as@hotmail.com>
2020-07-19 20:03:52 -0300

    Firs commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+window.addEventListener('load', () => {
+
+    let frequency = document.querySelector('#frequency')
+    let podcastImage = document.querySelector('.podcastImage')
+    let mhz = document.querySelector('#mhz')
+    let description = document.querySelector('.description')
+
+    console.log(podcastImage)
+
+    frequency.addEventListener('input', searchChannels)
+
+    let podcastChannels = podcast.map(channel => {
+        return {
+            name: channel.name,
+            image: channel.image,
+            frequency: channel.frequency,
+            description: channel.description,
+        }
+    })
+
+    function searchChannels() {
+        console.log(frequency.value)
+
+        podcastChannels.forEach(channel => {
+            if (channel.frequency === frequency.value) {
+                mhz.innerText = `${channel.frequency}`
+                console.log(`./img/${channel.image}.jpg`)
+                podcastImage.style.backgroundImage = `url("./img/${channel.image}.jpg")`
+                description.textContent = channel.description
+
+            }
+
+        })
+
+
+    }
+
+
+
+
+
+}
+)
